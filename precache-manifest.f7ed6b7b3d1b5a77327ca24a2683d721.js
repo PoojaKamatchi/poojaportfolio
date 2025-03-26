7efb3289b2a4ec8b3109cc8216bc7a45745df173
PoojaKamatchi <poojamuralipooja248@gmail.com>
2025-03-26 14:53:10 +0530

    Updates

---
+++ precache-manifest.f7ed6b7b3d1b5a77327ca24a2683d721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af6c7d51cf39cb46efc157176fced6f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "391b0d273899b494f6bd",
+    "url": "/resume/static/css/main.da714ca0.chunk.css"
+  },
+  {
+    "revision": "9c75925412f55e3f71dd",
+    "url": "/resume/static/js/2.fe7d4390.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/resume/static/js/2.fe7d4390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/resume/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "391b0d273899b494f6bd",
+    "url": "/resume/static/js/main.0266bd9d.chunk.js"
+  },
+  {
+    "revision": "012cab885a14a7a5512a",
+    "url": "/resume/static/js/runtime-main.8641ce74.js"
+  }
+]);
