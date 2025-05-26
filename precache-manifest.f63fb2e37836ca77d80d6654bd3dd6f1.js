52f98f498848e45408da24fd57a95e38e1ed01c9
PoojaKamatchi <poojamuralipooja248@gmail.com>
2025-05-26 20:43:22 +0530

    Updates

---
+++ precache-manifest.f63fb2e37836ca77d80d6654bd3dd6f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754db840ddfbc23c89265654a2d8de0f",
+    "url": "/poojaportfolio/index.html"
+  },
+  {
+    "revision": "8391b5655f3e14359409",
+    "url": "/poojaportfolio/static/css/main.077b7c59.chunk.css"
+  },
+  {
+    "revision": "ce6bd1d78905378435bd",
+    "url": "/poojaportfolio/static/js/2.28527a0d.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/poojaportfolio/static/js/2.28527a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbf512de136ab4d3d82",
+    "url": "/poojaportfolio/static/js/3.54f2bb70.chunk.js"
+  },
+  {
+    "revision": "8391b5655f3e14359409",
+    "url": "/poojaportfolio/static/js/main.86db9b9f.chunk.js"
+  },
+  {
+    "revision": "6a0fd0402540988885c9",
+    "url": "/poojaportfolio/static/js/runtime-main.7a6c4a93.js"
+  }
+]);
